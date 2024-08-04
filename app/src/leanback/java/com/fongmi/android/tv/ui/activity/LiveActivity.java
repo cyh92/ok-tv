63c121858c540440deb19c234e0b1ed95e2bbd68
cyh92 <cyh92@live.cn>
2024-08-04 18:03:27 +0800

    live

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
@@ -652,7 +652,7 @@ public class LiveActivity extends BaseActivity implements Clock.Callback, GroupP
     private void setInfo() {
         mViewModel.getEpg(mChannel);
         mBinding.widget.play.setText("");
-        mChannel.loadLogo(mBinding.widget.logo);
+//        mChannel.loadLogo(mBinding.widget.logo);
         mBinding.widget.name.setText(mChannel.getName());
         mBinding.widget.title.setText(mChannel.getName());
         mBinding.display.title.setText(mChannel.getName());
