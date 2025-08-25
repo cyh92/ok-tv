18fa0ef9b1e86d91786178eb7af50f7dde1b36c0
筱筱 <cyh92@live.cn>
2025-08-25 12:35:41 +0800

    更换下载链接

--- catvod/src/main/java/com/github/catvod/utils/Github.java
+++ catvod/src/main/java/com/github/catvod/utils/Github.java
@@ -2,17 +2,17 @@ package com.github.catvod.utils;
 
 public class Github {
 
-    public static final String URL = "https://raw.gitcode.com/cyh92/Release/raw/fongmi";
+    public static final String URL = "https://gitee.com/cyh92/ok-tv/releases/download";
 
     private static String getUrl(String path, String name) {
         return URL + "/" + path + "/" + name;
     }
 
     public static String getJson(boolean dev, String name) {
-        return getUrl("apk/" + (dev ? "dev" : "release"), name + ".json");
+        return getUrl((dev ? "dev" : "release"), name + ".json");
     }
 
     public static String getApk(boolean dev, String name) {
-        return getUrl("apk/" + (dev ? "dev" : "release"), name + ".apk");
+        return getUrl((dev ? "dev" : "release"), name + ".apk");
     }
 }
