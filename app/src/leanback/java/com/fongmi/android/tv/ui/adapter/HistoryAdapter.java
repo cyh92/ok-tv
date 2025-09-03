9b25d6380fae300ac9d31922bae865781aa537bc
cyh92 <cyh92@live.cn>
2025-09-04 06:22:58 +0800

    同步更新

--- app/src/leanback/java/com/fongmi/android/tv/ui/adapter/HistoryAdapter.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/adapter/HistoryAdapter.java
@@ -102,7 +102,7 @@ public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHold
         holder.binding.remark.setVisibility(delete ? View.GONE : View.VISIBLE);
         holder.binding.delete.setVisibility(!delete ? View.GONE : View.VISIBLE);
         holder.binding.remark.setText(ResUtil.getString(R.string.vod_last, item.getVodRemarks()));
-        ImgUtil.loadVod(item.getVodName(), item.getVodPic(), holder.binding.image);
+        ImgUtil.load(item.getVodName(), item.getVodPic(), holder.binding.image);
     }
 
     private void setFocusListener(AdapterVodBinding binding) {
