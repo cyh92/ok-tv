b4debb7283892ce9ff6e5b35f4ea1f62ac60b55d
cyh92 <cyh92@live.cn>
2025-09-07 16:19:37 +0800

    站点页面布局

--- app/src/main/java/com/fongmi/android/tv/Setting.java
+++ app/src/main/java/com/fongmi/android/tv/Setting.java
@@ -122,7 +122,7 @@ public class Setting {
     }
 
     public static int getSiteMode() {
-        return Prefers.getInt("site_mode");
+        return Prefers.getInt("site_mode",1);
     }
 
     public static void putSiteMode(int mode) {
