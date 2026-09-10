7d41d968bc0d4fcca09b7db1a7c01a927d538eb1
筱筱细雨凉生颊 <cyh92@live.cn>
2026-09-10 07:50:29 +0000

    优化vod界面焦点定位问题

--- app/src/leanback/java/com/fongmi/android/tv/ui/adapter/TypeAdapter.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/adapter/TypeAdapter.java
@@ -53,10 +53,7 @@ public class TypeAdapter extends RecyclerView.Adapter<TypeAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Class item = mItems.get(position);
-        holder.binding.text.setText(item.getTypeName());
-        holder.binding.getRoot().setSelected(item.isSelected());
-        holder.binding.getRoot().setNextFocusUpId(R.id.title);
-        holder.binding.getRoot().setNextFocusDownId(R.id.recycler);
+        holder.binding.text.setText(item.getTypeName());        
         holder.binding.text.setCompoundDrawablePadding(ResUtil.dp2px(4));
         holder.binding.text.setCompoundDrawablesWithIntrinsicBounds(0, 0, getIcon(item), 0);
         holder.binding.text.setListener(() -> mListener.onRefresh(item));
